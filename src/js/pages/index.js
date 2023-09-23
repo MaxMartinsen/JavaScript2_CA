b124f16b3a4818822856927146aae9dde479be96
Max Martinsen <111891491+MaxMartinsen@users.noreply.github.com>
2023-09-23 11:24:20 +0200

    Create Redirect for index.js
    Check if the user has logged in before

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,6 +1,7 @@
 import { signInButton, signUpButton } from '../components/buttons.js';
 
 document.addEventListener('DOMContentLoaded', function() {
+    // Handle signInButton click
     if (signInButton) {
         signInButton.addEventListener('click', function() {
             window.location.href = '../src/pages/login/login.html';
@@ -8,6 +9,8 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         console.error('signInButton element not found!');
     }
+
+    // Handle signUpButton click
     if (signUpButton) {
         signUpButton.addEventListener('click', function() {
             window.location.href = '../src/pages/register/index.html';
@@ -15,4 +18,19 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         console.error('signUpButton element not found!');
     }
+
+    // Check if the user has logged in before
+    const hasLoggedInBefore = localStorage.getItem('hasLoggedInBefore');
+    if (hasLoggedInBefore) {
+        // Redirect to the login page after a delay of 2 seconds
+        setTimeout(() => {
+            window.location.href = '../src/pages/login/login.html';
+        }, 1500);
+    } else {
+        // Redirect to the registration page
+        setTimeout(() => {
+            window.location.href = '../src/pages/register/index.html';
+        }, 1500);
+    }
 });
+
