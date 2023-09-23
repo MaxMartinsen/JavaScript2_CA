398dbb45d651885deac212f4c6ff1c6395ff0a70
Max Martinsen <111891491+MaxMartinsen@users.noreply.github.com>
2023-09-23 09:51:07 +0200

    Create a click event listener to the button (sign in and sign up)

---
+++ src/js/pages/login.js
@@ -0,0 +1,8 @@
+// Get the button element by its ID
+const signUpButton = document.getElementById('sign_up-button');
+
+// Add a click event listener to the button
+signUpButton.addEventListener('click', function() {
+    // Navigate to the desired page
+    window.location.href = '../../pages/register/index.html';
+});
--- src/js/pages/register.js
+++ src/js/pages/register.js
@@ -4,3 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("form");
     form.addEventListener("submit", registerForm);
 });
+
+// Get the button element by its ID
+const signInButton = document.getElementById('sign_in-button');
+
+// Add a click event listener to the button
+signInButton.addEventListener('click', function() {
+    // Navigate to the desired page
+    window.location.href = '../../pages/login/login.html';
+});
