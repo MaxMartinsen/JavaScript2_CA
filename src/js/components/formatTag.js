36976bccc0d21e69fa72757bc84e0ff0c479500d
Max Martinsen <max.martinsen@hotmail.com>
2023-10-03 07:34:19 +0200

    Update formatTag.js

--- src/js/components/formatTag.js
+++ src/js/components/formatTag.js
@@ -1,8 +1,10 @@
 export function formatTags(tags) {
     return tags.map(tag => {
+        tag = tag.toLowerCase().trim();
         if (!tag.startsWith('#')) {
             return '#' + tag;
         }
         return tag;
     });
 };
+
