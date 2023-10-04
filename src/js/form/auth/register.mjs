4b1ba05e9cee3f4b448ff01eb692e0be9638b479
Max Martinsen <max.martinsen@hotmail.com>
2023-10-04 08:17:12 +0200

    Create clear localStorage

--- src/js/form/auth/register.mjs
+++ src/js/form/auth/register.mjs
@@ -2,10 +2,6 @@ import { API_BASE_URL, API_VERSION, REGISTER_ENDPOINT } from '/src/js/api/url.mj
 import { authUser } from '/src/js/request/request.mjs';
 import { isValidUserName, isValidSocialEmail, isValidPassword } from '/src/js/validation/validation.mjs';
 
-/**
- * Handles the registration form submission.
- * @param {Event} event - The form submission event.
- */
 export function registerForm(event) {
     event.preventDefault();
 
@@ -13,24 +9,24 @@ export function registerForm(event) {
     const name = document.getElementById("nameInput").value;
     const password = document.getElementById("passwordInput").value;
 
-    // Check if the email is valid
     if (!isValidSocialEmail(email)) {
         console.error("Invalid Noroff email address.");
         return;
     }
-    // Check if the password is valid
+
     if (!isValidPassword(password)) {
         console.error("Password must be at least 8 characters long.");
         return;
     }
-    // Check if the username is valid
+
     if (!isValidUserName(name)) {
         console.error("Username should not contain any punctuation other than underscore (_).");
         return;
     }
 
-    const userToRegister = { name, email, password };
+    localStorage.clear();
 
+    const userToRegister = { name, email, password };
     const registerUrl = `${API_BASE_URL}${API_VERSION}${REGISTER_ENDPOINT}`;
 
     authUser(registerUrl, userToRegister)
--- src/js/pages/register.mjs
+++ src/js/pages/register.mjs
@@ -4,6 +4,7 @@ import { signInButton } from '/src/js/components/buttons.mjs';
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("form");
     form.addEventListener("submit", registerForm);
+    
     if (signInButton) {
         signInButton.addEventListener('click', function() {
             window.location.href = '../../pages/login/index.html';
