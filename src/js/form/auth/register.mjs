95b65be9487102fd1a65c844d7fb3e33f9c25c20
Max Martinsen <max.martinsen@hotmail.com>
2023-10-05 14:46:01 +0200

    Update register.mjs

--- src/js/form/auth/register.mjs
+++ src/js/form/auth/register.mjs
@@ -1,3 +1,14 @@
+/**
+ * Handles the registration form submission, validates the input, and attempts to register the user.
+ * If registration is successful, the user is redirected to the login page.
+ * If there's an error during registration, an error modal is displayed.
+ * @module form/auth/register
+ * @async
+ * @function
+ * @param {Event} event - The submit event triggered by the registration form.
+ * @throws Will log an error to the console if there's an issue during registration.
+ */
+
 import { API_BASE_URL, API_VERSION, REGISTER_ENDPOINT } from "/src/js/api/url.mjs";
 import { authUser } from "/src/js/request/request.mjs";
 import { validateForm } from "/src/js/utils/utils.mjs";
