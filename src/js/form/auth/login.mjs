b35a79de6ca223c22f83d808b20e940b94f7a44f
Max Martinsen <max.martinsen@hotmail.com>
2023-10-05 11:50:44 +0200

    Add validation

--- src/js/form/auth/login.mjs
+++ src/js/form/auth/login.mjs
@@ -1,38 +1,45 @@
-import { API_BASE_URL, API_VERSION, LOGIN_ENDPOINT } from '/src/js/api/url.mjs';
-import { authUser } from '/src/js/request/request.mjs';
-import { isValidSocialEmail, isValidPassword } from '/src/js/validation/validation.mjs';
+import { API_BASE_URL, API_VERSION, LOGIN_ENDPOINT } from "/src/js/api/url.mjs";
+import { authUser } from "/src/js/request/request.mjs";
+import {
+  isValidSocialEmail,
+  isValidPassword,
+} from "/src/js/validation/validation.mjs";
 
-/**
- * Handles the login form submission.
- * @param {Event} event - The form submission event.
- */
 export async function loginForm(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const email = document.getElementById("emailInput").value;
-    const password = document.getElementById("passwordInput").value;
+  const email = document.getElementById("emailInput").value;
+  const password = document.getElementById("passwordInput").value;
 
-    // Check if the email is valid
-    if (!isValidSocialEmail(email)) {
-        console.error("Invalid Noroff email address.");
-        return;
-    }
-    // Check if the password is valid
-    if (!isValidPassword(password)) {
-        console.error("Password must be at least 8 characters long.");
-        return;
-    }
+  // Check if the email is valid
+  if (!isValidSocialEmail(email)) {
+    document.getElementById("emailError").textContent =
+      "Invalid Noroff email address.";
+    return;
+  } else {
+    document.getElementById("emailError").textContent = "";
+  }
 
-    const userCredentials = { email, password };
+  // Check if the password is valid
+  if (!isValidPassword(password)) {
+    console.error("Password must be at least 8 characters long.");
+    document.getElementById("passwordError").textContent =
+      "Password must be at least 8 characters long.";
+    return;
+  } else {
+    document.getElementById("passwordError").textContent = "";
+  }
 
-    const loginUrl = `${API_BASE_URL}${API_VERSION}${LOGIN_ENDPOINT}`;
+  const userCredentials = { email, password };
 
-    try {
-        const data = await authUser(loginUrl, userCredentials);
-        localStorage.setItem('accessToken', data.accessToken);
-        localStorage.setItem('userName', data.name);
-        window.location.href = '/src/pages/profile/index.html';
-    } catch (error) {
-        console.error("There was an error logging in:", error);
-    }
-};
+  const loginUrl = `${API_BASE_URL}${API_VERSION}${LOGIN_ENDPOINT}`;
+
+  try {
+    const data = await authUser(loginUrl, userCredentials);
+    localStorage.setItem("accessToken", data.accessToken);
+    localStorage.setItem("userName", data.name);
+    window.location.href = "/src/pages/profile/index.html";
+  } catch (error) {
+    console.error("There was an error logging in:", error);
+  }
+}
--- src/js/form/auth/register.mjs
+++ src/js/form/auth/register.mjs
@@ -1,38 +1,50 @@
-import { API_BASE_URL, API_VERSION, REGISTER_ENDPOINT } from '/src/js/api/url.mjs';
-import { authUser } from '/src/js/request/request.mjs';
-import { isValidUserName, isValidSocialEmail, isValidPassword } from '/src/js/validation/validation.mjs';
+import { API_BASE_URL, API_VERSION, REGISTER_ENDPOINT } from "/src/js/api/url.mjs";
+import { authUser } from "/src/js/request/request.mjs";
+import { isValidUserName, isValidSocialEmail, isValidPassword } from "/src/js/validation/validation.mjs";
 
 export async function registerForm(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const email = document.getElementById("emailInput").value;
-    const name = document.getElementById("nameInput").value;
-    const password = document.getElementById("passwordInput").value;
+  const email = document.getElementById("emailInput").value;
+  const name = document.getElementById("nameInput").value;
+  const password = document.getElementById("passwordInput").value;
 
-    if (!isValidSocialEmail(email)) {
-        console.error("Invalid Noroff email address.");
-        return;
-    }
+  // Check if the email is valid
+  if (!isValidSocialEmail(email)) {
+    document.getElementById("emailError").textContent =
+      "Invalid Noroff email address.";
+    return;
+  } else {
+    document.getElementById("emailError").textContent = "";
+  }
 
-    if (!isValidPassword(password)) {
-        console.error("Password must be at least 8 characters long.");
-        return;
-    }
+  // Check if the password is valid
+  if (!isValidPassword(password)) {
+    document.getElementById("passwordError").textContent =
+      "Password must be at least 8 characters long.";
+    return;
+  } else {
+    document.getElementById("passwordError").textContent = "";
+  }
 
-    if (!isValidUserName(name)) {
-        console.error("Username should not contain any punctuation other than underscore (_).");
-        return;
-    }
+  // Check if the username is valid
+  if (!isValidUserName(name)) {
+    document.getElementById("nameError").textContent = "Username should not contain any punctuation other than underscore (_).";
+    return;
+  } else {
+    document.getElementById("nameError").textContent = "";
+  }
 
-    const userToRegister = { name, email, password };
-    const registerUrl = `${API_BASE_URL}${API_VERSION}${REGISTER_ENDPOINT}`;
 
-    try {
-        const data = await authUser(registerUrl, userToRegister);
-        localStorage.setItem('accessToken', data.accessToken);
-        localStorage.setItem('userName', data.name);
-        window.location.href = '/src/pages/login/index.html';
-    } catch (error) {
-        console.error("There was an error registering:", error);
-    }
-};
+  const userToRegister = { name, email, password };
+  const registerUrl = `${API_BASE_URL}${API_VERSION}${REGISTER_ENDPOINT}`;
+
+  try {
+    const data = await authUser(registerUrl, userToRegister);
+    localStorage.setItem("accessToken", data.accessToken);
+    localStorage.setItem("userName", data.name);
+    window.location.href = "/src/pages/login/index.html";
+  } catch (error) {
+    console.error("There was an error registering:", error);
+  }
+}
