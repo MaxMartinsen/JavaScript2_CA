0e4fbe084953db53dbe44b4c076cda458660eba3
Max Martinsen <max.martinsen@hotmail.com>
2023-10-05 15:16:38 +0200

    Update edit.mjs

--- src/js/pages/edit.mjs
+++ src/js/pages/edit.mjs
@@ -2,18 +2,16 @@
  * Once the DOM is fully loaded, this script initializes the edit page by displaying
  * the post for editing and setting up the post edit functionality.
  * @module pages/edit
+ * Display the post details for editing.
+ * displayPostForEdit();
+ * Initialize the post editing functionality.
+ * editPost();
  */
 
 import { editPost } from "/src/js/form/post/editPost.mjs";
 import { displayPostForEdit } from "/src/js/display/displayPostForEdit.mjs";
 
 document.addEventListener('DOMContentLoaded', () => {
-    /**
-     * Display the post details for editing.
-     */
     displayPostForEdit();
-    /**
-     * Initialize the post editing functionality.
-     */
     editPost();
 });
