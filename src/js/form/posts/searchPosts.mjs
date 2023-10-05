67703acc99a4f64db24a76ba494fb37ee442d681
Max Martinsen <max.martinsen@hotmail.com>
2023-10-05 15:09:09 +0200

    Update searchPosts.mjs

--- src/js/form/posts/searchPosts.mjs
+++ src/js/form/posts/searchPosts.mjs
@@ -1,3 +1,11 @@
+/**
+ * Searches for posts based on a given query. The search checks if the query exists
+ * in the post's title or body. The results are then displayed using the `displayPosts` function.
+ * @module form/posts/searchPosts
+ * @function
+ * @param {string} query - The search term used to filter posts.
+ * @throws Will log an error to the console if there's an issue during the search.
+ */
 import { displayPosts } from '/src/js/display/displayPosts.mjs';
 import { fetchGetPosts } from '/src/js/request/fetch.mjs';
 
