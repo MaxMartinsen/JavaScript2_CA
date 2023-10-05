ebab1b4247eae32fa26ced0bb9e93aee419e223c
Max Martinsen <max.martinsen@hotmail.com>
2023-10-05 14:38:50 +0200

    Update displayPostsByProfile.mjs

--- src/js/display/displayPostsByProfile.mjs
+++ src/js/display/displayPostsByProfile.mjs
@@ -1,3 +1,11 @@
+/**
+ * Displays a list of posts associated with a specific user profile.
+ * @module display/displayPostsByProfile
+ * @async
+ * @function
+ * @param {string} userName - The username of the profile whose posts are to be displayed.
+ * @throws Will throw an error if there's an issue fetching the posts or rendering them.
+ */
 import { PROFILES_ENDPOINT } from '/src/js/api/url.mjs';
 import { get } from '/src/js/request/request.mjs';
 import { displayPosts } from '/src/js/display/displayPosts.mjs';
