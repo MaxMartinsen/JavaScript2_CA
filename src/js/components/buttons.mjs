c4873f097c78db921bfb35d9ab7bb3e67732b8b5
Max Martinsen <max.martinsen@hotmail.com>
2023-10-05 14:12:42 +0200

    Update buttons.mjs

--- src/js/components/buttons.mjs
+++ src/js/components/buttons.mjs
@@ -1,5 +1,14 @@
-// Get the button element by its ID
+/**
+ * @module components/buttons
+ * @type {HTMLElement}
+ * Represents the sign-in button element.
+ * signInButton = document.getElementById('sign_in-button');
+ * Represents the sign-up button element.
+ * signUpButton = document.getElementById('sign_up-button');
+ */
+
 const signInButton = document.getElementById('sign_in-button');
+
 const signUpButton = document.getElementById('sign_up-button');
 
-export {signInButton , signUpButton};
+export { signInButton, signUpButton };
