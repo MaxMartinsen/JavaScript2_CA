ea3a5f2fa8b518ee42671b059b54bbb9489b09c2
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 07:59:26 +0200

    Update auth.mjs

--- src/js/request/auth.mjs
+++ src/js/request/auth.mjs
@@ -1,3 +1,14 @@
+/**
+ * Authenticates a user by sending their data to a specified URL.
+ *
+ * @param {string} url - The URL endpoint to which the authentication request should be sent.
+ * @param {Object} userData - The user's data to be sent for authentication.
+ * @param {string} userData.email - The user's email.
+ * @param {string} userData.password - The user's password.
+ * @returns {Promise<Object>} - A promise that resolves to the response data from the server.
+ * @throws {Error} - Throws an error if the HTTP request is not successful or other exceptions occur.
+ */
+
 export async function authUser(url, userData) {
     try {
         const postData = {
