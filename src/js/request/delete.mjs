6cfa6d08c7801c97737df91f889f9292d08b01a1
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 08:02:54 +0200

    Update delete.mjs

--- src/js/request/delete.mjs
+++ src/js/request/delete.mjs
@@ -1,5 +1,11 @@
 import { API_BASE_URL, API_VERSION, POSTS_ENDPOINT } from '/src/js/api/url.mjs';
-
+/**
+ * Deletes a post by its ID.
+ *
+ * @param {string|number} postId - The ID of the post to be deleted.
+ * @throws {Error} - Throws an error if the user is not authenticated or if the HTTP request fails.
+ * @returns {Promise<void>} - A promise that resolves when the post is successfully deleted.
+ */
 export async function del(postId) {
     const accessToken = localStorage.getItem('accessToken');
     if (!accessToken) {
