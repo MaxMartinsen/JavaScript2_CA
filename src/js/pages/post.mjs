ee8ed54017f02b13accb6ea0a9aba0f029e0bdb3
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 08:37:56 +0200

    Update post.mjs

--- src/js/pages/post.mjs
+++ src/js/pages/post.mjs
@@ -1,9 +1,9 @@
+import { displayPost } from "/src/js/display/displayPost.mjs";
 /**
  * @module pages/post
  * Once the DOM is fully loaded, this script initializes the post page by invoking
  * the displayPost function to render the post details.
  */
-import { displayPost } from "/src/js/display/displayPost.mjs";
 document.addEventListener('DOMContentLoaded', async function() {
     displayPost();
 
