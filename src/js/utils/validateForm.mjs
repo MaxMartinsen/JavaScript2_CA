6553f849aec6657cc738e054663857fe2dba81d9
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 09:14:54 +0200

    Update validateForm.mjs

--- src/js/utils/validateForm.mjs
+++ src/js/utils/validateForm.mjs
@@ -1,5 +1,19 @@
 import { isValidUserName, isValidSocialEmail, isValidPassword } from "/src/js/validation/validation.mjs";
 
+/**
+ * Utility functions for form validation.
+ * @module validateForm
+ * Validates form fields based on the given type.
+ * 
+ * @function
+ * @name validateForm
+ * @param {string} type - The type of form to validate. Expected values are 'login' or 'register'.
+ * @param {string} email - The email address to validate.
+ * @param {string} [name] - The username to validate. Required only for 'register' type.
+ * @param {string} password - The password to validate.
+ * @returns {boolean} Returns true if all validations pass, otherwise false.
+ * @throws Will throw an error if the validation functions from the imported module throw an error.
+ */
 export function validateForm(type, email, name, password) {
     if (!isValidSocialEmail(email)) {
         document.getElementById("emailError").textContent = "Invalid Noroff email address.";
