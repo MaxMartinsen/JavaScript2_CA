eed0fa7a1fce5979b76db7fb0206cff0db7785f9
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 09:14:32 +0200

    Update errorHandling.mjs

--- src/js/utils/errorHandling.mjs
+++ src/js/utils/errorHandling.mjs
@@ -1,3 +1,12 @@
+/**
+ * Utility functions related to error handling.
+ * @module errorHandling
+ * Shows an error modal to the user. When the modal is hidden, it clears the local storage and reloads the page.
+ * This function assumes that there's an element with the ID 'errorModal' in the DOM and that Bootstrap's Modal component is available.
+ * 
+ * @function
+ * @name showErrorModal
+ */
 export function showErrorModal() {
     const errorModal = new bootstrap.Modal(document.getElementById('errorModal'));
     errorModal.show();
