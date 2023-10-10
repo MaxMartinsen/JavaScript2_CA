5c8c8730dfa40c3af4beaca06ea279cbe9ff3fb8
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 08:10:34 +0200

    Update fetch.mjs

--- src/js/request/fetch.mjs
+++ src/js/request/fetch.mjs
@@ -2,9 +2,15 @@ import { POSTS_ENDPOINT, PROFILES_ENDPOINT, QUERY_PARAMETERS } from '/src/js/api
 import { get } from "/src/js/request/request.mjs";
 /**
  * Fetches a post by its ID.
- *
+ * fetchGetPostById(postId)
  * @param {string|number} postId - The ID of the post to be fetched.
  * @returns {Promise<Object>} - A promise that resolves to the fetched post.
+ * Fetches all posts.
+ * fetchGetPosts()
+ * @returns {Promise<Array<Object>>} - A promise that resolves to an array of posts.
+ * Fetches all profiles.
+ * fetchGetProfiles()
+ * @returns {Promise<Array<Object>>} - A promise that resolves to an array of profiles.
  */
 
 export async function fetchGetPostById(postId) {
