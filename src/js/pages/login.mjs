635ba325f991a6d37670de110a6617a2c130ea38
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 08:38:06 +0200

    Update login.mjs

--- src/js/pages/login.mjs
+++ src/js/pages/login.mjs
@@ -1,13 +1,11 @@
+import { loginForm } from '/src/js/form/auth/login.mjs';
+import { signUpButton } from "/src/js/components/buttons.mjs";
 /**
  * @module pages/login
  * Once the DOM is fully loaded, this script initializes the login page by setting up
  * event listeners for the form submission and signUpButton. It handles the login form
  * submission and redirects users who want to sign up to the registration page.
  */
-
-import { loginForm } from '/src/js/form/auth/login.mjs';
-import { signUpButton } from "/src/js/components/buttons.mjs";
-
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector("form");
     form.addEventListener("submit", loginForm);
