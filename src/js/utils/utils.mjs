60a1c231cb70abdbd7a14df4e4e4cfc2a5472d1d
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 09:14:52 +0200

    Update utils.mjs

--- src/js/utils/utils.mjs
+++ src/js/utils/utils.mjs
@@ -1,9 +1,44 @@
+/**
+ * Utility functions for various tasks such as form validation, date formatting, 
+ * tag formatting, and UI filter setup.
+ * @module utils
+ */
+
+/**
+ * @description Imports and re-exports utility functions from various utility modules.
+ */
+
+/** 
+ * @function
+ * @name validateForm
+ * @see {@link module:validateForm}
+ */
 export { validateForm } from "/src/js/utils/validateForm.mjs";
 
+/** 
+ * @function
+ * @name setupFilterListeners
+ * @see {@link module:setupFilterListeners}
+ */
 export { setupFilterListeners } from "/src/js/utils/setupFilterListeners.mjs";
 
+/** 
+ * @function
+ * @name setActiveFilter
+ * @see {@link module:setActiveFilter}
+ */
 export { setActiveFilter } from "/src/js/utils/setActiveFilter.mjs";
 
+/** 
+ * @function
+ * @name formatDateAndTime
+ * @see {@link module:formatDateAndTime}
+ */
 export { formatDateAndTime } from "/src/js/utils/formatDateAndTime.mjs";
 
+/** 
+ * @function
+ * @name formatTags
+ * @see {@link module:formatTags}
+ */
 export { formatTags } from '/src/js/utils/formatTags.mjs';
