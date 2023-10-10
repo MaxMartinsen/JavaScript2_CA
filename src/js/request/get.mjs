159c18688cf8278034ae2d1eadf0985e62faa7b8
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 08:18:33 +0200

    Update get.mjs

--- src/js/request/get.mjs
+++ src/js/request/get.mjs
@@ -1,5 +1,15 @@
-import { API_BASE_URL, API_VERSION } from '/src/js/api/url.mjs';
-
+import { API_BASE_URL, API_VERSION, } from '/src/js/api/url.mjs';
+/**
+ * Common query parameters to be used with API requests.
+ * These parameters are used to fetch additional related data like author, comments, and reactions.
+ * @type {string}
+ * Sends a GET request to the specified endpoint and returns the response data.
+ * 
+ * @param {string} endpoint - The API endpoint to which the GET request should be sent.
+ * @param {string} [queryParameters=''] - Additional query parameters to be appended to the endpoint.
+ * @returns {Promise<Object>} - Returns a promise that resolves with the response data.
+ * @throws {Error} - Throws an error if the user is not authenticated or if the HTTP request fails.
+ */
 const QUERY_PARAMETERS = '?_author=true&_comments=true&_reactions=true';
 
 export async function get(endpoint, queryParameters = '') {
