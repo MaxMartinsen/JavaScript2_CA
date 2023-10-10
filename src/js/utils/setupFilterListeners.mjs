c6add1dfd85ebcd8dc018585bb9a8f02952c3643
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 09:14:49 +0200

    Update setupFilterListeners.mjs

--- src/js/utils/setupFilterListeners.mjs
+++ src/js/utils/setupFilterListeners.mjs
@@ -1,5 +1,22 @@
 import { setActiveFilter } from '/src/js/utils/utils.mjs';
 
+/**
+ * Utility functions related to setting up filter event listeners.
+ * @module setupFilterListeners
+ * 
+ * Sets up event listeners for filter elements in the UI. When a filter is clicked, 
+ * it triggers the provided display function and sets the clicked filter as active.
+ * 
+ * @function
+ * @name setupFilterListeners
+ * @param {Function} displayPostsFunction - The function to be called when a filter is clicked. 
+ * This function is responsible for displaying posts based on the selected filter.
+ * @example
+ * 
+ * // Assuming there are filter elements with IDs 'filter-publications', 'filter-popular', etc.
+ * // and a function named 'displayPosts' that takes a filter type as an argument.
+ * setupFilterListeners(displayPosts);  
+ */
 export function setupFilterListeners(displayPostsFunction) {
     document.getElementById('filter-publications').addEventListener('click', (e) => {
         e.preventDefault();
