2cd273fcee3ce7ccba6129e03d3ee9e18e2df611
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 09:14:42 +0200

    Update formatTags.mjs

--- src/js/utils/formatTags.mjs
+++ src/js/utils/formatTags.mjs
@@ -1,4 +1,17 @@
-export function formatTags(tags) {
+
+/**
+ * Utility functions related to tag formatting.
+ * @module formatTags
+ * Formats an array of tags to ensure they are lowercase, trimmed, and start with a '#'.
+ * 
+ * @function
+ * @name formatTags
+ * @param {string[]} tags - An array of tag strings to be formatted.
+ * @returns {string[]} An array of formatted tag strings.
+ * @example
+ * 
+ * Outputs: ["#test", "#ok", "#post"]
+ */export function formatTags(tags) {
     return tags.map(tag => {
         tag = tag.toLowerCase().trim();
         if (!tag.startsWith('#')) {
