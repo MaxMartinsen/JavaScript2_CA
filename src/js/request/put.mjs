e8e9754b435390b67278af8c84e9172b4c6c1bbb
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 08:37:58 +0200

    Update put.mjs

--- src/js/request/put.mjs
+++ src/js/request/put.mjs
@@ -1,5 +1,12 @@
 import { API_BASE_URL, API_VERSION, POSTS_ENDPOINT } from '/src/js/api/url.mjs';
-
+/**
+ * Sends a PUT request to the specified endpoint with the provided post data.
+ * 
+ * @param {string|number} postId - The ID of the post to be updated.
+ * @param {Object} postData - The data to be sent in the body of the PUT request.
+ * @returns {Promise<Object>} - Returns a promise that resolves with the updated post data.
+ * @throws {Error} - Throws an error if the user is not authenticated or if the HTTP request fails.
+ */
 export async function put(postId, postData) {
     const accessToken = localStorage.getItem('accessToken');
     if (!accessToken) {
