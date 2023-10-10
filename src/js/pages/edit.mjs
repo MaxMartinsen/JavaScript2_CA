b2eb4ad8423b0ab2b0d93069b91fd0649e81eaab
Max Martinsen <max.martinsen@hotmail.com>
2023-10-10 08:38:36 +0200

    Update edit.mjs

--- src/js/pages/edit.mjs
+++ src/js/pages/edit.mjs
@@ -1,3 +1,5 @@
+import { editPost } from "/src/js/form/post/editPost.mjs";
+import { displayPostForEdit } from "/src/js/display/displayPostForEdit.mjs";
 /**
  * Once the DOM is fully loaded, this script initializes the edit page by displaying
  * the post for editing and setting up the post edit functionality.
@@ -7,10 +9,6 @@
  * Initialize the post editing functionality.
  * editPost();
  */
-
-import { editPost } from "/src/js/form/post/editPost.mjs";
-import { displayPostForEdit } from "/src/js/display/displayPostForEdit.mjs";
-
 document.addEventListener('DOMContentLoaded', () => {
     displayPostForEdit();
     editPost();
